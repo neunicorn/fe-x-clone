533a7773bb7d03898f63356905afcb35c04fd64a
Muhamad Zulfan <73164719+neunicorn@users.noreply.github.com>
2024-08-08 21:13:08 +0700

    porject structure

---
+++ src/config/Routes/index.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
